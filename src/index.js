1e32393a2579b2bdd2ba7593fc11c6fb9df4bc47
anyi lozano <anyi.lozano2119@gmail.com>
2022-03-22 12:05:13 -0500

    commit merch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./app";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./styles/App.scss";
 import reportWebVitals from "./reportWebVitals";
 
 const root = document.getElementById('root');
